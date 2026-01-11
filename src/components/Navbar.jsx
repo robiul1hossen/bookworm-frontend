2e8e35d0ddd0a76b64bec529caa4bcfa7b77a0a0
Mohammad Robiul Hossen <robim6225@gmail.com>
2026-01-12 00:52:36 +0600

    initialize the project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,77 @@
+import { NavLink } from "react-router";
+
+const Navbar = () => {
+  const links = (
+    <>
+      <NavLink to="/">
+        <li className="font-semibold mx-2">Home</li>
+      </NavLink>
+      <NavLink to="/all-books">
+        <li className="font-semibold mx-2">All Books</li>
+      </NavLink>
+      <NavLink to="/about">
+        <li className="font-semibold mx-2">About</li>
+      </NavLink>
+
+      <>
+        <NavLink to="/auth/login">
+          <li className="font-semibold mx-2">Logout</li>
+        </NavLink>
+        {/* <img
+            src={user?.photoURL}
+            alt=""
+            className="w-8 h-8 rounded-full cursor-pointer"
+            referrerPolicy="no-referrer"
+          /> */}
+      </>
+
+      <>
+        <NavLink to="/auth/login">
+          <li className="font-semibold mx-2">Login</li>
+        </NavLink>
+        <NavLink to="/auth/register">
+          <li className="font-semibold mx-2">Register</li>
+        </NavLink>
+      </>
+    </>
+  );
+  return (
+    <div className="navbar bg-base-100 shadow-sm px-4">
+      <div className="navbar-start">
+        <a className="text-xl">BOOKWORMS</a>
+      </div>
+
+      <div className="navbar-end">
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{links}</ul>
+        </div>
+        {/* <a className="btn">Button</a> */}
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor">
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex="-1"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+            {links}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,8 @@
+import { createBrowserRouter } from "react-router";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h2>hello world</h2>,
+  },
+]);
